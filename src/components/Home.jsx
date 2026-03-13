370feb701d1eae7a1c58fe40717e168cfe6e2b42
TiwariSudhanshu <sudhanshutiwari9836@gmail.com>
2026-03-14 00:45:23 +0530

    Add NavBar component to Home and configure Tailwind CSS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 const Home=()=>{
     return(
+        <>
         <h1>Home</h1>
+        </>
     )
 }
 export default Home
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,22 @@ import {Link} from "react-router-dom"
 
 const NavBar=()=>{
     return( 
-        <div className="" >
-            <div className="">
-               <Link to='/'>Home</Link>
-               <Link to='/about'>About</Link>
-               <Link to='/contact'>Contact</Link>
-            </div>
+        // <div className="" >
+        //     <div className="">
+        //        <Link to='/'>Home</Link>
+        //        <Link to='/about'>About</Link>
+        //        <Link to='/contact'>Contact</Link>
+        //     </div>
         
             
+        // </div>
+        <div className="bg-white flex justify-around h-20 border-b-2 items-center">
+           <div className="text-2xl font-bold">Beginner Dev</div> 
+           <div className="flex justify-around w-[20%]">
+              <Link to='/'>Home</Link>
+              <Link to='/about'>About</Link>
+             <Link to='/contact'>Contact</Link>
+           </div>
         </div>
     )
 }
